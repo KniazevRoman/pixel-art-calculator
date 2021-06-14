a30e351da3249f3ff929bbbaf6e6a7c6d712c147
Roman Kniazev <kniazev.re@gmail.com>
2021-06-14 14:34:55 +0300

    calculator works

--- script.js
+++ script.js
@@ -13,8 +13,236 @@ document.addEventListener("DOMContentLoaded", ()=> {
     for (let button of longButtons) {
         button.style.height = getComputedStyle(buttons[0]).width;
     }
-
 });
 
 /* Logic */
 
+createCalc();
+
+function createCalc() {
+    let powered = false;
+    let screen = document.querySelector('.calculator__screen');
+    startPowerButton();
+
+    let operand1 = '';
+    let operator = '';
+    let operand2 = '';
+    let result = '';
+
+    let startNewInput = false;
+    let newCalculation = false;
+
+    function startPowerButton() {
+        let powerButton = document.querySelector('.calculator__controls-button_power');
+        let powerIndicator = document.querySelector('.calculator__controls-power-indicator');
+        powerButton.addEventListener('click', ()=> {
+            if (!powered) {
+                powered = true;
+                powerIndicator.style.backgroundColor = 'rgb(9, 240, 59)';
+                screen.textContent = '0';
+                operand1 = '';
+                operator = '';
+                operand2 = '';
+                result = '';
+                startNumberButtons();
+                startClearButton();
+                startOperatorButtons();
+                startEqualButton();
+            } else {
+                powered = false;
+                powerIndicator.style.backgroundColor = 'rgb(50, 50, 104)';
+                screen.textContent = '';
+                operand1 = '';
+                operator = '';
+                operand2 = '';
+                result = '';
+                terminateNumberButtons();
+                terminateClearButton();
+                terminateOperatorButtons();
+                terminateEqualButton();
+            }
+        });
+    }
+
+    /* Process user unput */
+
+    function processNumber(event) {
+        if (screen.textContent.length == 10 && !startNewInput && !newCalculation) {
+            console.log('Input is ignored. No free space on the screen');
+            return;
+        } else if(screen.textContent.indexOf('.') != -1 && event.target.dataset.number == '.' && !startNewInput && !newCalculation) {
+            console.log('Input is ignored. The dot has already been inputed');
+            return;
+        }
+
+        if(newCalculation) {
+            operand1 = '';
+            operator = '';
+            operand2 = '';
+            result = '';
+            screen.textContent = '';
+
+            newCalculation = false;
+        }
+
+        if(!operator) {
+            if (!operand1) {
+                screen.textContent = event.target.dataset.number;
+                operand1 += event.target.dataset.number;
+            } else {
+                screen.textContent += event.target.dataset.number;
+                operand1 += event.target.dataset.number;
+            }
+        } else {
+            if (startNewInput) {
+                startNewInput = false;
+                screen.textContent = event.target.dataset.number;
+                operand2 += event.target.dataset.number;
+            } else {
+                screen.textContent += event.target.dataset.number;
+                operand2 += event.target.dataset.number;
+            }
+        }
+        
+        
+    }
+
+    function clearInput() {
+        screen.textContent = '0';
+        if(newCalculation) {
+            operand1 = '';
+            operator = '';
+            operand2 = '';
+            result = '';
+        }else if(!operator) {
+            operand1 = '';
+        } else {
+            operand2 = '';
+        }
+    }
+
+    function processOperator(event) {
+        startNewInput = true;
+
+        if(!operator || newCalculation) {
+            operator = event.target.textContent;
+            operand2 = '';
+            newCalculation = false;
+        } else {
+            if (operator == '+') {
+                result = +operand1 + +operand2;
+                screen.textContent = checkForOverflow(result);
+
+                operator = event.target.textContent;
+                operand1 = result;
+                operand2 = '';
+            } else if(operator == '-') {
+                result = operand1 - operand2;
+                screen.textContent = checkForOverflow(result);
+
+                operator = event.target.textContent;
+                operand1 = result;
+                operand2 = '';
+            } else if(operator == '*') {
+                result = operand1 * operand2;
+                screen.textContent = checkForOverflow(result);
+
+                operator = event.target.textContent;
+                operand1 = result;
+                operand2 = '';
+            } else if(operator == '/') {
+                result = operand1 / operand2;
+                screen.textContent = checkForOverflow(result);
+
+                operator = event.target.textContent;
+                operand1 = result;
+                operand2 = '';
+            }
+        }
+    }
+
+    function processEqual() {
+        newCalculation = true;
+        if(!operator) {
+            return;
+        } else if (operator == '+') {
+            result = +operand1 + +operand2;
+            screen.textContent = checkForOverflow(result);
+            operand1 = result;
+        } else if(operator == '-') {
+            result = operand1 - operand2;
+            screen.textContent = checkForOverflow(result);
+            operand1 = result;
+        } else if(operator == '*') {
+            result = operand1 * operand2;
+            screen.textContent = checkForOverflow(result);
+            operand1 = result;
+        } else if(operator == '/') {
+            result = operand1 / operand2;
+            screen.textContent = checkForOverflow(result);
+            operand1 = result;
+        }
+    }
+
+    /* Round the output to fit in the screen */
+
+    function checkForOverflow(output) {
+        if (output.toString().length >= 10) {
+            return +output.toFixed(10-Math.round(output).toString().length-1);
+        } else {
+            return +output;
+        }
+    }
+
+    /* Turn on the calculator */
+
+    function startNumberButtons() {
+        let numberButtons = document.querySelectorAll('[data-number]');
+        for (let numberButton of numberButtons) {
+            numberButton.addEventListener('click', processNumber);
+        }
+    }
+
+    function startClearButton() {
+        let clearButton = document.querySelector('.calculator__controls-button_clear');
+        clearButton.addEventListener('click', clearInput);
+    }
+
+    function startOperatorButtons() {
+        let operatorButtons = document.querySelectorAll('.calculator__controls-button_operator');
+        for (let operatorButton of operatorButtons) {
+            operatorButton.addEventListener('click', processOperator);
+        }
+    }
+
+    function startEqualButton() {
+        let equalButton = document.querySelector('.calculator__controls-button_result');
+        equalButton.addEventListener('click', processEqual);
+    }
+
+    /* Turn off the calculator */
+
+    function terminateNumberButtons() {
+        let numberButtons = document.querySelectorAll('[data-number]');
+        for (let numberButton of numberButtons) {
+            numberButton.removeEventListener('click', processNumber);
+        }
+    }
+
+    function terminateClearButton() {
+        let clearButton = document.querySelector('.calculator__controls-button_clear');
+        clearButton.removeEventListener('click', clearInput);
+    }
+
+    function terminateOperatorButtons() {
+        let operatorButtons = document.querySelectorAll('.calculator__controls-button_operator');
+        for (let operatorButton of operatorButtons) {
+            operatorButton.removeEventListener('click', processOperator);
+        }
+    }
+
+    function terminateEqualButton() {
+        let equalButton = document.querySelector('.calculator__controls-button_result');
+        equalButton.removeEventListener('click', processEqual);
+    }
+}
